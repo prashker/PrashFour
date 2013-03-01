64ad813b1877f5cf71553952e6fc38025d218383
Samuel Prashker <github@prashker.net>
2013-03-01 11:45:17 -0500

    highlightCheck fixes

--- public/js/models.js
+++ public/js/models.js
@@ -11,7 +11,8 @@ var Message = Backbone.Model.extend({
             this.set({text: this.get('raw')});
         }
 
-        //Temporary solution to make unread highlights work again
+        //highlightCheck was moved out of models.js to utils.js, this is a highlight fix
+        //check if nick is present in RAW
         if (this.get('type') === 'message' && this.get('raw').search('\\b' + irc.me.get('nick') + '\\b') !== -1) {
             this.set({highlight: true});
         }
@@ -54,24 +55,20 @@ var ChatWindow = Backbone.Model.extend({
 
     setUnread: function(msg) {
         if (this.get('active')) {
+            //It isn't unread if it is the active window
             return;
         }
-        
-        var sig = false;
         
         // Increment unread messages
         if (msg.get('type') === 'message' || msg.get('type') === 'pm') {
             this.set({unread: this.get('unread') + 1});
         }
         if (this.get('type') === 'pm') { 
-            signal = true;
+            //PM Handler
         }
         if (msg.get('highlight')) {
               this.set({unreadHighlights: this.get('unreadHighlights') + 1});
-              signal = true;
         }
-        // All PMs & Highlights
-        if (sig) this.trigger('forMe', 'message');
     }
 });
 
